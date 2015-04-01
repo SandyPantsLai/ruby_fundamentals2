a014b4a2c2cc1e33e2b288e3878f04b1f003f33e
Sandy Lai <sandywslai@gmail.com>
2015-04-01 15:34:05 -0400

    Corrected comments to describe method.[log]

--- exercise5.rb
+++ exercise5.rb
@@ -1,15 +1,4 @@
-# Create a short program that converts Fahrenheit temperatures to Celsius in a file called exercise5.rb.
-
-# Start with prompting the user for a temperature in F. Once you get the input, call the method while using your input as a parameter.
-
-# Your method should:
-
-# have one parameter: the temperature in Fahrenheit
-# do the conversion with this formula: (C = (F - 32) x 5/9)
-# ensure that the parameter you pass in is a number by converting it with to_i
-# Output the result in a full sentence using string interpolation.
-
-# Don't forget to commit your progress as you go along. Once you're done, commit and push it to github.
+#Convert Fahrenheit to Celsius
 
 def f_to_c(degrees_f)
 	degrees_f = degrees_f.to_i
