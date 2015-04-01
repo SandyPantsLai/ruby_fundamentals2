49de2bbaf0a82f152aa39c797adf545c2b07d113
Sandy Lai <sandywslai@gmail.com>
2015-04-01 16:10:17 -0400

    Added method to display enrollment in each cohort [log]

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,12 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+#Display how many students are in each cohort
+def enrollment(list)
+	list.each do |key, value|
+		puts key.to_s + ": " + value.to_s + " students"
+	end
+end
+
+enrollment(students)
