e6c65c9763a2b096477653eaf1bd13f492970bb5
Sandy Lai <sandywslai@gmail.com>
2015-04-01 16:01:53 -0400

    Added dashed line below each list to improve output and improved output messages so it is clearer to see what part of the exercise is what.

--- exercise6.rb
+++ exercise6.rb
@@ -3,6 +3,7 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 #Method to list groceries with *
 def list_groceries(list)
 	list.each {|item| puts "* #{item}"}
+	puts "---------------------------"
 end
 
 list_groceries(grocery_list)
@@ -13,7 +14,7 @@ grocery_list.push("rice")
 list_groceries(grocery_list)
 
 #Output number of list items
-puts grocery_list.count
+puts "There are " + grocery_list.count.to_s + " items on the grocery list."
 
 #Check if bananas are on the list
 if grocery_list.include?("bananas")
@@ -24,7 +25,7 @@ end
 
 #Display second item of list
 
-puts grocery_list[1]
+puts "The second grocery item on the list is " + grocery_list[1] + "."
 
 #Alphabetize list
 grocery_list.sort!
